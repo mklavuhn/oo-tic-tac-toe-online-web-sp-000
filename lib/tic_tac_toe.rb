4b3732303c2889adbafd8292a7aee03e43f7314c
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-10 18:44:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     cp = current_player
-    if valid_move?
+    if valid_move?(index)
       move(index, cp)
       display_board
     else
